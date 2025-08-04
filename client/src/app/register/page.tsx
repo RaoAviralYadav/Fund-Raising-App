eec4f6498177ff534e8efe77d2aa3e687df32961
RaoAviralYadav <24f3000436@ds.study.iitm.ac.in>
2025-08-05 03:57:11 +0530

    Login/Registration Page live

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <main className="pt-20 min-h-screen bg-gradient-to-br from-orange-50 via-yellow-100 to-white px-4 flex items-center justify-center">
       <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
-        <h1 className="text-2xl font-bold mb-6 text-center text-black">Login</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center text-black">Register</h1>
         <form className="space-y-4">
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full px-4 py-2 border border-gray-400 rounded focus:outline-none focus:ring"
+          />
           <input
             type="email"
             placeholder="Email"
@@ -20,13 +25,13 @@ export default function LoginPage() {
             type="submit"
             className="w-full px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
           >
-            Login
+            Register
           </button>
         </form>
         <p className="mt-4 text-sm text-center text-gray-600">
-          Dont have an account?{' '}
-          <Link href="/register" className="text-green-600 hover:underline">
-            Register
+          Already have an account?{' '}
+          <Link href="/login" className="text-green-600 hover:underline">
+            Login
           </Link>
         </p>
       </div>
